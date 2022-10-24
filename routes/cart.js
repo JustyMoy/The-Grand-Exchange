3c68fd9229f175db33e9bc93f6e4858f54e13034
Justin Moy <moy.justin94@gmail.com>
2022-10-23 19:22:05 -0500

    first commit

---
+++ routes/cart.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+
+router.put("/addCart/:id", postsController.addCart)
+
+
+
+
+module.exports = router;
